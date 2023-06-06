18e71f2da132e12b956560bfd51d2f23ae12d012
Alan <ahw9f@virginia.edu>
2023-06-06 15:41:05 -0400

    saving download

--- code/download_all_shapefiles.py
+++ code/download_all_shapefiles.py
@@ -3,6 +3,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import requests
+import signal, os
 
 """
 Download all county blocks
@@ -11,7 +12,15 @@ Download all county blocks
 # wget -P ../data/shapefiles/ https://www2.census.gov/geo/tiger/TIGER2020PL/LAYER/TABBLOCK/2020/tl_2020_13121_tabblock20.zip
 
 
+def handler(signum, frame):
+    print("Signal handler called with signal", signum)
+
+
 def main():
+    assert os.path.isdir(
+        "../data/shapefiles/"
+    ), "No directory found at ../data/shapefiles/"
+
     # identify all files to download
     r = requests.get(
         "https://www2.census.gov/geo/tiger/TIGER2020PL/LAYER/TABBLOCK/2020/"
@@ -19,17 +28,20 @@ def main():
     soup = BeautifulSoup(r.content, "html.parser")
     counties = []
     for a in soup.find_all("a", href=True):
-        if len(a["href"]) == 28 and a["href"].split(".")[-1] == "zip":
+        if a["href"].split(".")[-1] == "zip":
             print(a["href"])
             counties.append(a["href"])
 
     for county in tqdm(counties):
         os.system(
-            "wget -P ../data/shapefiles/ https://www2.census.gov/geo/tiger/TIGER2020PL/LAYER/TABBLOCK/2020/%s"
+            "wget -nc -P ../data/shapefiles/ https://www2.census.gov/geo/tiger/TIGER2020PL/LAYER/TABBLOCK/2020/%s"
             % county
         )
-        assert os.path.isfile(os.path.join("../data/shapefiles", county))
+        filepath = os.path.join("../data/shapefiles", county)
+        assert os.path.isfile(filepath), filepath
 
 
 if __name__ == "__main__":
+    # Set the signal handler
+    signal.signal(signal.SIGINT, handler)
     main()
